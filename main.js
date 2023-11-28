9031723cdcc8c2e48e26830196f653522f531e08
yahyaferhi <yhyaferhi12@gmail.com>
2023-11-28 11:14:40 +0100

    done

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+    var buttons = document.querySelectorAll(".foods button");
+
+    
+    buttons.forEach(function (button) {
+        button.addEventListener("click", function () {
+    
+            var foodD = this.parentNode;
+
+    
+            var foodT = foodD.querySelector("h4").textContent;
+
+            alert("You selected: " + foodT);
+        });
+    });
+});
 
